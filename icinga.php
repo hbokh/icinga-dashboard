c9b58bddb272a7a237af421c965c58ec5360531b
Henk Bokhoven <hbokhoven@gmail.com>
2011-10-21 11:25:31 +0200

    Minor fix

--- icinga.php
+++ icinga.php
@@ -1,6 +1,6 @@
 <?php 
     $refreshvalue = 10; //value in seconds to refresh page
-    $pagetitle = "Operations Nagios Dashboard";
+    $pagetitle = "Icinga Dashboard";
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
